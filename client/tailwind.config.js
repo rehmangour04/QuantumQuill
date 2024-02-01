c5441f45e634d876d8470efbec1ba40c39546d09
Ataur Rehman <rehmangour04@gmail.com>
2024-02-02 00:00:19 +0530

    create a header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,9 +4,13 @@
  */
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };

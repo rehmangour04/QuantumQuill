fa072ec23099c818caa4697faf7a7a8a97075208
Ataur Rehman <rehmangour04@gmail.com>
2024-04-23 22:31:14 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 /** @format */
 
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("mongodb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
